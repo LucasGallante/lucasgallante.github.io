2f53f78da2faf682c554f8422d6b6a8416c0bf3c
Lucas Leal <lucas.leal@sensedia.com>
2020-08-10 17:58:26 -0300

    Updates

---
+++ precache-manifest.a1639586c37ad2cfe0280613fd2da7d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc6706241390a0a93b4c44e4865bdf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9124ca4848559e72b71b",
+    "url": "/static/js/main.0ce0aa80.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);

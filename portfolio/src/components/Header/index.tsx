bd07446ae478fba0d1ab537d32b14c110fe21cba
Lucas Leal <ceni.lucas@gmail.com>
2021-01-09 21:03:11 -0300

    Fixing menu navigation

--- portfolio/src/components/Header/index.tsx
+++ portfolio/src/components/Header/index.tsx
@@ -16,8 +16,8 @@ const Header = () => {
                             <Nav className="ml-auto">
                                 <Nav.Link href="#about">About</Nav.Link>
                                 <Nav.Link href="#skills">Skills</Nav.Link>
-                                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                                <Nav.Link href="#contact">Contact</Nav.Link>
+                                <Nav.Link href="#projects">Projects</Nav.Link>
+                                <Nav.Link href="#footer">Contact</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
